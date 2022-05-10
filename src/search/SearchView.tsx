75b291c347ba0e2761562ecacb11bd4069a486ab
22-04-80 <s220480@student.uek.krakow.pl>
2022-05-10 22:02:45 +0200

    Add incorrect apiKey handling

--- src/search/SearchView.tsx
+++ src/search/SearchView.tsx
@@ -22,6 +22,7 @@ interface SearchViewState {
   query: string;
   searchResults: any[]; // TODO
   loading: boolean;
+  error: boolean;
 }
 
 export class SearchView extends React.Component<SearchViewProps, SearchViewState> {
@@ -30,7 +31,7 @@ export class SearchView extends React.Component<SearchViewProps, SearchViewState
   constructor(props: SearchViewProps) {
     super(props);
 
-    this.state = {query: "", searchResults: [], loading: false};
+    this.state = {query: "", searchResults: [], loading: false, error: false};
 
     this.kg = new KnowledgeGraph(props.apiKey);
   }
@@ -44,18 +45,23 @@ export class SearchView extends React.Component<SearchViewProps, SearchViewState
   protected onPerformSearch = async (): Promise<void> => {
     const {query} = this.state;
 
-    this.setState({searchResults: [], loading: true})
+    this.setState({searchResults: [], loading: true, error: false})
 
-    const response = await this.kg.search(query);
-    const responseJson = await response.json();
-    console.log(responseJson);
-
-    this.setState({searchResults: responseJson.itemListElement, loading: false})
+    try {
+      const response = await this.kg.search(query);
+      const responseJson = await response.json();
+      console.log(responseJson);
+  
+      this.setState({searchResults: responseJson.itemListElement, loading: false, error: false});
+    } catch(error) {
+      this.setState({searchResults: [], loading: false, error: true});
+      console.error(error);
+    }
   }
 
   public render () {
       const {onSelectResult} = this.props;
-      const {query, loading, searchResults} = this.state;
+      const {query, loading, searchResults, error} = this.state;
       console.log({searchResults})
 
       return (
@@ -98,7 +104,37 @@ export class SearchView extends React.Component<SearchViewProps, SearchViewState
               </Box>
               <Container sx={{ py: 8 }} maxWidth="md">
                 <Grid container spacing={4}>
-                  {searchResults.map((searchResult) => (
+                  {!error && searchResults?.length === 0 && (
+                    <Grid
+                      container
+                      spacing={0}
+                      direction="column"
+                      alignItems="center"
+                    >
+                      <Typography variant="h5" align="center" color="text.secondary" paragraph>
+                        No results
+                      </Typography>
+                    </Grid>
+                  )}
+
+                  {(error || !searchResults) && (
+                    <Grid
+                      container
+                      spacing={0}
+                      direction="column"
+                      alignItems="center"
+                    >
+                      <Typography variant="h5" align="center" paragraph>
+                        Incorrect API Key
+                      </Typography>
+                      <Typography variant="h6" color="text.secondary" align="center" paragraph>
+                        Please refresh the page and try again.
+                      </Typography>
+                    </Grid>
+                  )}
+
+
+                  {!error && searchResults?.map((searchResult) => (
                     <SearchResult
                       key={searchResult.result["@id"]}
                       searchResult={searchResult}
